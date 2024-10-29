d9e577a3038354647af945b038d860bc780fb373
Yoshigonnawin <celishevxelart@gmail.com>
2024-10-29 23:50:52 +0500

    refactor: учет регистра при like поиске

--- Поиск однофамильцев.sql
+++ Поиск однофамильцев.sql
@@ -11,33 +11,26 @@ declare
 	l_name_prep text; 
 begin 
 	select * into staff_info from staff s where s.id = staff_id;
+	l_name_prep = lower(staff_info.l_name);
 	case 
-		when RIGHT(staff_info.l_name, 2)='ый'
-		then 
-			l_name_prep = regexp_replace(staff_info.l_name, 'ый$', '', 'g');
-			l_name_prep = concat(l_name_prep,'%');
-			
-		when RIGHT(staff_info.l_name, 2)='ая'
-		then l_name_prep = regexp_replace(staff_info.l_name, 'ая$', '', 'g');
-			l_name_prep = concat(l_name_prep,'%');
-			
-		when RIGHT(staff_info.l_name, 1)='а'
-		then l_name_prep = regexp_replace(staff_info.l_name, 'а$', '', 'g');
-			l_name_prep = concat(l_name_prep,'%');
-			
-		when RIGHT(staff_info.l_name, 2)='ой'
-		then l_name_prep = regexp_replace(staff_info.l_name, 'ой$', '', 'g');
-			l_name_prep = concat(l_name_prep,'%');
-		else
-			l_name_prep = concat(staff_info.l_name,'%');
+		when RIGHT(l_name_prep, 2)='ый'
+		then l_name_prep = regexp_replace(l_name_prep, 'ый$', '', 'g');
+		when RIGHT(l_name_prep, 2)='ая'
+		then l_name_prep = regexp_replace(l_name_prep, 'ая$', '', 'g');
+		when RIGHT(l_name_prep, 1)='а'
+		then l_name_prep = regexp_replace(l_name_prep, 'а$', '', 'g');
+		when RIGHT(l_name_prep, 2)='ой'
+		then l_name_prep = regexp_replace(l_name_prep, 'ой$', '', 'g');
+		else 
 	end case;
-	
+	l_name_prep = concat(l_name_prep,'%');
+
 	select concat(staff_info.f_name,' ',staff_info.s_name,' ',staff_info.l_name) as stf,
 	string_agg(concat(s.f_name,' ',s.s_name,' ',s.l_name),', ') as other
 	into outp 
 	from staff s 
 	where 1=1
-	and s.l_name like(l_name_prep)
+	and lower(s.l_name) like(l_name_prep)
 	and s.id != staff_id;
 	
 end$$LANGUAGE plpgsql;

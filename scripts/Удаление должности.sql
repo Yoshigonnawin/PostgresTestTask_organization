75eb3e00394f10a86573b6a272b5470065ca814f
Yoshigonnawin <celishevxelart@gmail.com>
2025-01-21 19:02:42 +0500

    refactor: Уходим от цикла for и оптимизируемся по памяти и количеству запросов к базе

--- scripts/Удаление должности.sql
+++ scripts/Удаление должности.sql
@@ -1,17 +1,14 @@
 CREATE OR replace FUNCTION delete_positions ( pos_ids int4[]
 ) returns void AS $$
     declare
-        pos_id int4;
         staff_ids bigint[];
     begin
-    for pos_id in(
-        select unnest(pos_ids) 
-    )
-    loop
-        select array_agg(ps.staff_id)::bigint[] into  staff_ids from position_staff ps where ps.position_id = pos_id;
-        delete from position_staff ps where ps.position_id = pos_id;
-        delete from positions p where p.id = pos_id;
 
-        perform delete_staff(staff_ids);
-    end loop;
+    select array_agg(ps.staff_id)::bigint[] into  staff_ids from position_staff ps where ps.position_id = any(pos_ids);
+
+    delete from position_staff ps where ps.position_id = any(pos_ids);
+    delete from positions p where p.id = any(pos_ids);
+
+    perform delete_staff(staff_ids);
+
 end; $$ language plpgsql;

b7ebf3ce4cc0351d0782584efbc04dd927329b78
Yoshigonnawin <celishevxelart@gmail.com>
2025-01-21 19:27:56 +0500

    refactor: уходим от цикла for, уменьшаем количество вставок в таблицу deltas

--- scripts/Удаление сотрудника.sql
+++ scripts/Удаление сотрудника.sql
@@ -1,31 +1,30 @@
 CREATE OR replace FUNCTION delete_staff ( staff_ids bigint[]
 ) returns void AS $$
 	declare
-    office_id bigint;
-    stff_id bigint;
+    office_id_num record;
 	begin
-		for stff_id in(
-            select * from unnest(staff_ids)
-	    )
-        loop 
-            delete from staff_hierarchy sh  
-            where sh.chief_staff_id = stff_id or sh.sub_staff_id = stff_id;
 
-            delete from department_staff ds where ds.staff_id = stff_id;
-            
-            delete from position_staff ps where ps.staff_id = stff_id;
-
-            select os.office_id into office_id from office_staff os  where os.staff_id = stff_id;
-            
-            if office_id notnull
-            then
-                delete from office_staff os  where os.staff_id = stff_id;
-                
-                INSERT INTO "server".deltas
-                ( value, object_id, delta_type_id)
-                VALUES( 1, office_id, 1);
-            end if;
-            
-            raise notice 'Staff deleted id = %',stff_id;
+        for office_id_num in (
+            select os.office_id,count(staff_id) as nums 
+            from office_staff os 
+            where os.staff_id = any(staff_ids)
+            group by os.office_id 
+        )
+        loop
+            INSERT INTO "server".deltas
+            ( value, object_id, delta_type_id)
+            VALUES( office_id_num.num, office_id_num.office_id, 1);
         end loop;
+        
+        delete from staff_hierarchy sh  
+        where sh.chief_staff_id = any(staff_ids) or sh.sub_staff_id = any(staff_ids);
+
+        delete from department_staff ds where ds.staff_id = any(staff_ids);
+            
+        delete from position_staff ps where ps.staff_id = any(staff_ids);
+
+        delete from office_staff os  where os.staff_id = any(staff_ids);
+            
+        raise notice 'Staff deleted id = %',staff_ids;
+     
 end; $$ language plpgsql;
